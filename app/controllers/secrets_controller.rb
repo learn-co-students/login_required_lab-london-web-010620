975b0e0238a41565fe720c48e88a7b3dd6b772ad
samuelkyprianou <samuelkyprianou@gmail.com>
2020-02-06 15:49:35 +0000

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,5 @@
+class SecretsController < ApplicationController
+  def show
+    redirect_to root_path unless session.include? :name
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'sessions#new'
+  get 'secrets/show', to: 'secrets#show'
+  resources :sessions, only: [:new, :create]
+  post 'sessions/destroy', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
